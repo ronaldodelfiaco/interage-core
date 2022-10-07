de0bd04d682e9d392143ec3f1dba98f614d199ce
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-10-07 09:42:24 -0300

    Melhorado as configurações do router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventosComponent } from './eventos/eventos.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { PagesLayoutComponent } from './layout/pages-layout/pages-layout.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SemPermissaoComponent } from './sem-permissao/sem-permissao.component';
 
 const routes: Routes = [
   {
-    path: 'eventos',
-    component: EventosComponent
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: EventosComponent
+      },
+      {
+        path: 'semPermissao', component: SemPermissaoComponent
+      },
+    ]
   },
   {
     path: '',
@@ -28,7 +40,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
