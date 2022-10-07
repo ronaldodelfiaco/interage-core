d5fa1e928e793ee58b58f6923669b855e6970320
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-10-07 09:47:24 -0300

    ajustes no serviço http e local storage

--- frontend/src/app/shared/services/connect-http.service.ts
+++ frontend/src/app/shared/services/connect-http.service.ts
@@ -1,8 +1,10 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { SortDirection } from "@angular/material/sort";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Usuario } from "../../login/usuario";
 import { LocalStorageService } from "./local-storage.service";
-
 
 // var localSevidor = "http://88.99.35.190:3000/" //Producao
 var localSevidor = environment.urlApi;  //Local
@@ -20,18 +22,140 @@ interface retObjectCallService {
   resposta: object
 }
 
+interface Eventos {
+  id_pessoa_criou: number,
+  id_evento_pai: number,
+  id_evento_anterior: number,
+  id_campanha: number,
+  listar_seus_eventos: boolean,
+  id_motivo: number,
+  id_status_evento: number,
+  id: number,
+  dt_criou: Date,
+  dt_prevista_resolucao: Date,
+  dt_para_exibir: Date,
+  id_pessoa_organograma: number,
+  id_usuario: number,
+  id_pessoa_receptor: number,
+  id_prioridade: number,
+  dt_visualizou: Date,
+  id_pessoa_visualizou: number,
+  dt_resolvido: Date,
+  id_pessoa_resolveu: number,
+  id_resp_motivo: number,
+  id_predicao: number,
+  id_canal: number,
+  id_telefone: number,
+  id_objecao: number,
+  excedeu_tentativas: boolean,
+  id_proposta: number,
+  tempo_gasto_resolveu_ss: number,
+  campanha: string,
+  pessoa_visualizou: string,
+  tempo_gasto_resolveu: string,
+  motivo: string,
+  telefone: string,
+  status: string,
+  status_evento_cor_stringo: string,
+  status_evento_icone: string,
+  pessoa_resolveu: string,
+  pessoa_criou: string,
+  exigi_aviso_leitura: string,
+  observacao_origem: string,
+  observacao_retorno: string,
+  tipodestino: string,
+  complemento_resposta: string,
+  destino: string,
+  resposta_motivo: string,
+  tempo: string,
+  cliente: string,
+  predicao: string,
+  prioridade: string,
+  objecao: string,
+  canal: string,
+}
+
+interface BancoEventos {
+  message: string;
+  body: {
+    table: Eventos[];
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectHTTPService {
   localStorage: LocalStorageService = new LocalStorageService();
-  callService(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
+  config: void;
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getCountEventos(options: optionsCallService): Observable<any> {
+    if (!options.naoExigeToken) {
+      const usuarioLogado = this.localStorage.get('usuarioLogado') as Usuario;
+      if (usuarioLogado != undefined) {
+        options.paramsService = {
+          ...options.paramsService,
+          // id_usuario: usuarioLogado.id,
+          // token: usuarioLogado.token
+          id_usuario: 44,
+          token: usuarioLogado.token
+        }
+      }
+    }
+
+    const paramsService = this._trataParamsService(options.paramsService);
+    const url = `${environment.urlApi}${options.service}${paramsService}`;
+    const body = `sql=select count(id) from view_eventos`;
+
+    return this._httpClient.post(encodeURI(url),
+      body,
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+      }
+    );
+  }
+
+  getEventosPage(options: optionsCallService, sort: string, order: SortDirection, page: number): Observable<BancoEventos> {
+    if (!options.naoExigeToken) {
+      const usuarioLogado = this.localStorage.get('usuarioLogado') as Usuario;
+      if (usuarioLogado != undefined) {
+        options.paramsService = {
+          ...options.paramsService,
+          // id_usuario: usuarioLogado.id,
+          // token: usuarioLogado.token
+          id_usuario: 44,
+          token: usuarioLogado.token
+        }
+      }
+    }
+
+    const paramsService = this._trataParamsService(options.paramsService)
+
+    const url = `${environment.urlApi}${options.service}${paramsService}`;
+    // const requestUrl = `${href}&sort=${sort}&order=${order}&page=${page + 1}`;
+    const body = `sql=select * from view_eventos ORDER BY ${sort} ${order} LIMIT 10 OFFSET ${10 * page}`;
+
+    return this._httpClient.post<BancoEventos>(encodeURI(url),
+      body,
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+      }
+    );
+  }
+
+  getUser(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
     return new Promise((resolve, reject) => {
       //TROCA DADOS SERVIDOR
-      const host = options.host || localSevidor;
+      const host = localSevidor;
 
-      const service = options.service
-      let url = `${host}${service}`
+      const service = options.service;
+
+      let url = `${host}${service}`;
+
       if (!options.naoExigeToken) {
         let usuarioLogado = this.localStorage.get('usuarioLogado') as Usuario;
         if (usuarioLogado != undefined) {
@@ -55,6 +179,8 @@ export class ConnectHTTPService {
           if (!selfXhttp.responseText) return resolve({ resposta: {} })
           else resolve({ resposta: JSON.parse(selfXhttp.responseText).usuarioLogado, error: '' })
         } else if (selfXhttp.status === 401) {
+          console.log(selfXhttp.responseText);
+
           resolve({ resposta: {}, error: selfXhttp.responseText === 'object' ? JSON.parse(selfXhttp.responseText) : selfXhttp.responseText })
         }
       }
@@ -68,6 +194,52 @@ export class ConnectHTTPService {
     })
   }
 
+  callService(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
+    return new Promise((resolve, reject) => {
+      //TROCA DADOS SERVIDOR
+      const host = localSevidor;
+
+      const service = options.service;
+
+      let url = `${host}${service}`;
+
+      if (!options.naoExigeToken) {
+        let usuarioLogado = this.localStorage.get('usuarioLogado') as Usuario;
+        if (usuarioLogado != undefined) {
+          options.paramsService = {
+            ...options.paramsService,
+            id_usuario: usuarioLogado.id,
+            token: usuarioLogado.token
+          }
+        }
+      }
+
+      if (options.paramsService) {
+        const paramsService = this._trataParamsService(options.paramsService)
+        url = `${url}${paramsService}`
+      }
+      const xhttp = new XMLHttpRequest()
+
+      xhttp.onload = function () {
+        const selfXhttp = this;
+        if (selfXhttp.status === 200) {
+          if (!selfXhttp.responseText) return resolve({ resposta: {} })
+          else resolve({ resposta: JSON.parse(selfXhttp.responseText), error: '' })
+        } else if (selfXhttp.status === 401) {
+          console.log(selfXhttp.responseText);
+
+          resolve({ resposta: {}, error: selfXhttp.responseText === 'object' ? JSON.parse(selfXhttp.responseText) : selfXhttp.responseText })
+        }
+      }
+
+      xhttp.onerror = (e) => {
+        reject(e)
+      }
+
+      xhttp.open("GET", url)
+      xhttp.send()
+    })
+  }
 
   // send file em json
   sendFile(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
@@ -119,7 +291,6 @@ export class ConnectHTTPService {
       xhttp.send(JSON.stringify(arquivo))
     })
   }
-
 
   uploadFiles(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
     if (!options.paramsService.arquivo) return { error: 'É necessário enviar o arquivo.', resposta: {} };
@@ -174,7 +345,6 @@ export class ConnectHTTPService {
       return `${o}=${paramsService[o]}`
     }).join('&')
   }
-
 
   postHHTP(options: optionsCallService): Promise<retObjectCallService> | retObjectCallService {
 
@@ -234,10 +404,4 @@ export class ConnectHTTPService {
       }))
     })
   }
-
-
-
-
 }
-
-
--- frontend/src/app/shared/services/local-storage.service.ts
+++ frontend/src/app/shared/services/local-storage.service.ts
@@ -11,25 +11,25 @@ export class LocalStorageService {
   public set(key: string, value: any) {
     if (this.storage) {
       if (value instanceof Date)
-        this.storage.setItem(`${key}`, value.toString());
+        this.storage.setItem(`${key}_date`, value.toString());
       else if (typeof value === 'object')
-        this.storage.setItem(`${key}`, JSON.stringify(value));
+        this.storage.setItem(`${key}_object`, JSON.stringify(value));
       else
-        this.storage.setItem(`${key}`, value);
+        this.storage.setItem(`${key}_string`, value);
       return true;
     }
     return false;
   }
 
   public get(key: string) {
-    let retDate = this.storage.getItem(`${key}`)
+    let retDate = this.storage.getItem(`${key}_date`)
     if (retDate)
       return new Date(retDate);
 
-    let retObject = this.storage.getItem(`${key}`)
+    let retObject = this.storage.getItem(`${key}_object`)
     if (retObject)
       return JSON.parse(retObject)
-    return this.storage.getItem(`${key}`)
+    return this.storage.getItem(`${key}_string`)
   }
 
   public clear() {
@@ -41,13 +41,14 @@ export class LocalStorageService {
   }
 
   public remove(key: string, value: string) {
+
     if (this.storage) {
       if (value == 'date')
-        localStorage.removeItem(`${key}`)
+        localStorage.removeItem(`${key}_date`)
       else if (value == 'object')
-        localStorage.removeItem(`${key}`)
+        localStorage.removeItem(`${key}_object`)
       else
-        localStorage.removeItem(`${key}`)
+        localStorage.removeItem(`${key}_string`)
     }
     return false;
   }
